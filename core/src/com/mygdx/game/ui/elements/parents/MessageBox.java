236e5d127fcb06b71682654f4d634988d1531cb5
aznoH333 <glentik23@gmail.com>
2022-10-03 09:14:20 +0200

    messages

--- core/src/com/mygdx/game/ui/elements/parents/MessageBox.java
+++ core/src/com/mygdx/game/ui/elements/parents/MessageBox.java
@@ -9,6 +9,8 @@ import com.mygdx.game.ui.interfaces.IUIElement;
 import com.mygdx.game.ui.interfaces.IUIParentElement;
 import com.mygdx.game.ui.interfaces.IUIUpdatable;
 
+import jdk.net.SocketFlow;
+
 public class MessageBox implements IUIParentElement, IUIUpdatable, IUIElement {
 
     private final float closedPosition = 1400;
@@ -18,6 +20,7 @@ public class MessageBox implements IUIParentElement, IUIUpdatable, IUIElement {
     private final static DrawingManager spr = DrawingManager.getINSTANCE();
     private UIActionStatus targetStatus = UIActionStatus.Closed;
     private long closeTimer = 0;
+    private float openingPercentage = 0;
 
     @Override
     public void uiOpen() {targetStatus = UIActionStatus.Open;}
@@ -40,17 +43,36 @@ public class MessageBox implements IUIParentElement, IUIUpdatable, IUIElement {
                 y -= (float) Math.ceil(Math.abs(y-openPosition)/ animationSpeed);
             }
             // close ui
-            else if (targetStatus == UIActionStatus.Closed){
+            else if (targetStatus == UIActionStatus.Closed && openingPercentage == 0){
                 y += (float) Math.max(Math.ceil(Math.abs(y-openPosition)/ animationSpeed),1);
                 if (y > closedPosition) y = closedPosition;
             }
         }
         if (Game.Time() == closeTimer) uiClose();
+
+
+        if (targetStatus == UIActionStatus.Closed && openingPercentage > 0){
+            openingPercentage-=0.04;
+            if (openingPercentage < 0) openingPercentage = 0;
+        }else if (getStatus() == UIActionStatus.Open && openingPercentage < 1){
+            openingPercentage+=0.04;
+            if (openingPercentage > 1) openingPercentage = 1;
+        }
     }
 
     @Override
     public void draw() {
-        spr.drawText(text, (Game.gameWorldWidth/2) - ((float) (text.length() * 25)/2), y, 3);
+        // borders
+        spr.draw("msgBox0", (Game.gameWorldWidth/2)-24 - (openingPercentage * ((Game.gameWorldWidth/2) - 64)), y, 3);
+        spr.draw("msgBox0", (Game.gameWorldWidth/2) + (openingPercentage * ((Game.gameWorldWidth/2) - 64)), y, 3);
+
+        // backdrop
+        for (float x = 0; x < (openingPercentage * ((Game.gameWorldWidth/2) - 64)); x+= 24){
+            spr.draw("msgBox1", (Game.gameWorldWidth/2) + x, y, 3);
+            spr.draw("msgBox1", (Game.gameWorldWidth/2) - 24 - x, y, 3);
+        }
+        if (openingPercentage == 1) spr.drawText(text, (Game.gameWorldWidth/2) - ((float) (text.length() * 25)/2), y  + 64, 3);
+
     }
 
     @Override
@@ -70,6 +92,6 @@ public class MessageBox implements IUIParentElement, IUIUpdatable, IUIElement {
     private String text = "";
     public void setText(String text){
         this.text = text;
-        closeTimer = Game.Time() + 90;
+        closeTimer = Game.Time() + 180;
     }
 }
